81991d11c775872025dbe14233e90ca8d80e590c
Lucas Marques <lucasg3.gdh@gmail.com>
2023-03-01 10:04:26 -0300

    Adicionando console log

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 console.log("Executando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+
+// console log de teste
+
+console.log("Enviando para o Github")
