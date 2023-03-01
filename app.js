13a82fb0d72b5ad7d1839091d83744d66418bafd
Lucas Marques <lucasg3.gdh@gmail.com>
2023-03-01 10:10:26 -0300

    Restaurando o app.js

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
 console.log("Executando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação está se conectando ao banco de dados")
-
-// console log de teste
-
-console.log("Enviando para o Github")
